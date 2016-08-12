929e3890cdbe9921b758041afef3a8c003b31653
Ivan Zorkin <qiik@live.ru>
2016-08-12 21:21:04 +0300

    Initial commit

---
+++ app/src/main/java/net/qiik/project/myapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package net.qiik.project.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import net.qiik.project.mylibrary.TestLibraryClass;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TestLibraryClass
+    }
+}
---
+++ mylibrary/src/main/java/net/qiik/project/mylibrary/TestLibraryClass.java
@@ -0,0 +1,8 @@
+package net.qiik.project.mylibrary;
+
+/**
+ * Created by NotePad1 on 12.08.2016.
+ */
+
+public class TestLibraryClass {
+}
